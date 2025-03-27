b0a3e76fa680dc206e433444aa48b6dd76d0d5d5
jtech555 <jtech55@gmail.com>
2025-03-27 09:23:34 -0400

    test

--- prototypes/platformer-final/Assets/IntroSceneManager.cs
+++ prototypes/platformer-final/Assets/IntroSceneManager.cs
@@ -1,6 +1,6 @@
 
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 public class IntroSceneManager : MonoBehaviour
 {
